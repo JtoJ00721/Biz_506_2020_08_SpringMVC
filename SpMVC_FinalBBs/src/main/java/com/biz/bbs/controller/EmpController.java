5d34f26039df9a1eb08710733ddf8f4d07fcbdcd
JtoJ00721 <moonlf2000@naver.com>
2020-12-02 13:51:41 +0900

    이제 정말 끝

--- SpMVC_FinalBBs/src/main/java/com/biz/bbs/controller/EmpController.java
+++ SpMVC_FinalBBs/src/main/java/com/biz/bbs/controller/EmpController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -28,7 +29,7 @@ public class EmpController {
 	}
 
 	@RequestMapping(value = "/select/{id}", method = RequestMethod.GET)
-	public EmployeesVO selectOne(@RequestParam Long id) {
+	public EmployeesVO selectOne(@PathVariable("id") Long id) {
 		
 		EmployeesVO empVO = empService.findById(id);
 
